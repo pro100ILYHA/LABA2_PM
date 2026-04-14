9428f3604482f1be71018801e56ce4416f537016
Subbotin <subbotinilya2007@gmail.com>
2026-04-14 23:07:45 +0300

    Subbotin, Добавлен тестовый класс MusicTrackTests

---
+++ UnitTestProject1/MusicTrackTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WindowsFormsApp1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class MusicTrackTests
+    {
+        [TestMethod]
+        public void ToString_VozvratCorrectFormat()
+        {
+            // Arrange
+            var track = new MusicTrack("Певец", "Песня", "Жанр", 2007);
+
+            // Act
+            var result = track.ToString();
+
+            // Assert
+            Assert.AreEqual("Певец - Песня (2007) [Жанр]", result);
+        }
+
+        [TestMethod]
+        public void Constructor_SohraneniyeUstanovlZnacheniy()
+        {
+            // Arrange
+            var testArtist = "КрутойПевец";
+            var testTitle = "КрутаяПесня";
+            var testGenre = "рокнрол";
+            var testYear = 2011;
+
+            // Act
+            var track = new MusicTrack(testArtist, testTitle, testGenre, testYear);
+
+            // Assert
+            Assert.AreEqual(testArtist, track.Artist);
+            Assert.AreEqual(testTitle, track.Title);
+            Assert.AreEqual(testGenre, track.Genre);
+            Assert.AreEqual(testYear, track.Year);
+        }
+    }
+}
