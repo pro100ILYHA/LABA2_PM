a844f7a0124f4220a4a377582b83ee85edc0bb1c
Subbotin <subbotinilya2007@gmail.com>
2026-04-15 00:42:48 +0300

    Subbotin, Добавлен тестовый класс MusicCollectionFormTests

---
+++ UnitTestProject1/MusicCollectionFormTests.cs
@@ -0,0 +1,40 @@
+﻿    using WindowsFormsApp1;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Windows.Forms;
+
+    namespace WindowsFormsApp1.Tests
+    {
+        [TestClass]
+        public class MusicCollectionFormUITests
+        {
+            private MusicCollectionForm _form;
+
+            [TestInitialize]
+            public void SetUp()
+            {
+                _form = new MusicCollectionForm();
+            }
+
+            [TestMethod]
+            public void Form_HasCorrectTitle()
+            {
+                // Assert
+                Assert.AreEqual("Управление музыкальной коллекцией", _form.Text);
+            }
+
+            [TestMethod]
+            public void Form_IsVisible()
+            {
+                // Assert
+                Assert.IsTrue(_form.Visible);
+            }
+
+            [TestMethod]
+            public void Form_HasCorrectSize()
+            {
+                // Assert
+                Assert.AreEqual(520, _form.Width);
+                Assert.AreEqual(400, _form.Height);
+            }
+        }
+    }
