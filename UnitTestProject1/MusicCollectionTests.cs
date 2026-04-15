ca8f3ef185bdaeea90416acecdc13d9d5b4aa38d
pro100ILYHA <subbotinilya2007@gmail.com>
2026-04-15 14:04:08 +0300

    Добавлен тестовый класс MusicCollectionTests а также изменен MusicCollection

---
+++ UnitTestProject1/MusicCollectionTests.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Forms;
+using WindowsFormsApp1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class MusicCollectionTests
+    {
+        [TestMethod]
+        public void LoadTracksPrivate()
+        {
+            var listViev = new ListView();
+            var collection = new MusicCollection(listViev);
+
+            var track = new MusicTrack("IlyaSubbotin", "pesnyaSubika", "jazz", 2007);
+            collection.AddTrack(track);
+
+            var tracks = collection.GetTracks();
+            var resultListView = collection.GetListView();
+
+            Assert.IsNotNull(resultListView);
+            Assert.AreEqual(1, tracks.Count);
+            Assert.AreEqual("IlyaSubbotin", tracks[0].Artist);
+        }
+
+        // Тест для метода AddTrack()
+        [TestMethod]
+        public void AddTrack_AddTrackToCollection()
+        {
+            // Arrange
+            var listView = new ListView();
+            var collection = new MusicCollection(listView);
+            var testTrack = new MusicTrack("IlyaSubbotin", "pesnyaSubika", "jazz", 2007);
+
+            // Act
+            collection.AddTrack(testTrack);
+            var tracks = collection.GetTracks();
+
+            // Assert
+            Assert.AreEqual(1, tracks.Count);
+            Assert.AreEqual("Hey Jude", tracks[0].Title);
+        }
+
+        // Тест для метода RemoveTrack()
+        [TestMethod]
+        public void RemoveTrack_RemoveTrack()
+        {
+            // Arrange
+            var listView = new ListView();
+            var collection = new MusicCollection(listView);
+            var testTrack = new MusicTrack("IlyaSubbotin", "pesnyaSubika", "jazz", 2007);
+            collection.AddTrack(testTrack);
+
+            // Act
+            collection.RemoveTrack(testTrack);
+            var tracks = collection.GetTracks();
+
+            // Assert
+            Assert.AreEqual(0, tracks.Count);
+        }
+
+        // Тест для метода SearchByArtist()
+        [TestMethod]
+        public void SearchByArtist_FindTrackByArtistName()
+        {
+            // Arrange
+            var listView = new ListView();
+            var collection = new MusicCollection(listView);
+            var testTrack = new MusicTrack("IlyaSubbotin", "pesnyaSubika", "jazz", 2007);
+            collection.AddTrack(testTrack);
+
+            // Act
+            collection.SearchByArtist("The Beatles");
+
+            // Assert
+            Assert.AreEqual(1, listView.Items.Count);
+            Assert.AreEqual("The Beatles", listView.Items[0].SubItems[0].Text);
+        }
+
+        // Тест для метода SortByYear()
+        [TestMethod]
+        public void SortByYear_SortTracksByYear()
+        {
+            // Arrange
+            var listView = new ListView();
+            var collection = new MusicCollection(listView);
+            var track1 = new MusicTrack("IlyaSubbotin", "pesnyaSubika", "jazz", 2007);
+            var track2 = new MusicTrack("Subbotin", "Subiktrack", "pop", 2001);
+            collection.AddTrack(track1);
+            collection.AddTrack(track2);
+
+            // Act
+            collection.SortByYear();
+
+            // Assert
+            Assert.AreEqual("2001", listView.Items[0].SubItems[3].Text);
+            Assert.AreEqual("2007", listView.Items[1].SubItems[3].Text);
+        }
+    }
+}
